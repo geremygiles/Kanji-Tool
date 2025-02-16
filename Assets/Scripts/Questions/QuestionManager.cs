b10bdad7266123c88756b77d7fb6858114c043b0
Geremy Giles <60160599+geremygiles@users.noreply.github.com>
2025-02-16 19:49:52 +0900

    Adds random question

--- Assets/Scripts/Questions/QuestionManager.cs
+++ Assets/Scripts/Questions/QuestionManager.cs
@@ -18,6 +18,11 @@ public class QuestionManager : MonoBehaviour
 
     private string randomKanji = "一七万三上下中九二五人今休何先入八六円出分前北十千午半南友右名四国土外大天女子学小山川左年後日時書月木本来東校母毎気水火父生男白百聞行西見話語読車金長間雨電食高";
 
+    void Start()
+    {
+        LoadQuestion();
+    }
+
     private void LoadQuestion() {
         // Choose a "random" question
         UpdateProbabilites();
@@ -26,9 +31,8 @@ public class QuestionManager : MonoBehaviour
         PickQuestion();
         UpdateImage();
 
-        // Load the answer(s) in a random location
-
-        // Load some extra answers
+        // Load the answer(s) in a random location, with the other options filled with random kanji.
+        UpdateOptions();
     }
 
     private void PickQuestion() {
@@ -54,22 +58,26 @@ public class QuestionManager : MonoBehaviour
     }
 
     private void UpdateOptions() {
-        List<int> optionIndexes = new List<int>() {1,2,3,4,5,6};
+        List<int> optionIndexes = new List<int>() {0,1,2,3,4,5};
 
         for (int i = 0; i < currentQuestion.kanji.Length; i++) {
             int random = Random.Range(0, optionIndexes.Count);
 
             Transform option = optionButtons.transform.GetChild(optionIndexes[random]).GetChild(0);
-            optionIndexes.Remove(random);
+            optionIndexes.RemoveAt(random);
+            Debug.Log("1 correct answer at " + random);
 
             option.gameObject.GetComponent<TextMeshProUGUI>().text = currentQuestion.kanji[i];
         }
 
         for (int i = 0; i < optionIndexes.Count; i++) {
             Transform option = optionButtons.transform.GetChild(optionIndexes[i]).GetChild(0);
-            optionIndexes.Remove(i);
 
-            option.gameObject.GetComponent<TextMeshProUGUI>().text = randomKanji[Random.Range(0, randomKanji.Length)].ToString();
+            string test = randomKanji[Random.Range(0, randomKanji.Length)].ToString();
+
+            Debug.Log("Filling option #" + optionIndexes[i] + " with " + test);
+
+            option.gameObject.GetComponent<TextMeshProUGUI>().text = test;
         }
 
     }
